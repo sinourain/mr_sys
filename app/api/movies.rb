d4738d935718b8b14631bfb7b4c091729e12814a
Luis Mario Urrea-Murillo <sinourain@gmail.com>
2019-09-29 11:13:49 -0500

    Add list and create requests to movies api and update the movies_spec

--- app/api/movies.rb
+++ app/api/movies.rb
@@ -2,7 +2,32 @@ class Movies < Grape::API
   prefix :api
   format :json
   
-  get :movies do
-    {movies: Movie.all}
+  resource :movies do
+    desc 'Return all movies by day name'
+    params do
+      requires :day_name, type: String, desc: 'Day name.'
+    end
+    get do
+      { movies: Movie.list_by_day_name(params[:day_name]) }
+    end
+    
+    desc 'Create a movie'
+    params do
+      requires :name, type: String, desc: 'Movie name.'
+      requires :description, type: String, desc: 'Movie description.'
+      requires :image_url, type: String, desc: 'Movie image URL.'
+    end
+    post do
+      create_movie = CreateMovie.new
+      create_movie.call(params) do |transaction|
+        transaction.success do |movie, presentation_days|
+          { movie: movie, presentation_days: presentation_days }
+        end
+
+        transaction.failure do |errors|
+          error!({ errors: errors }, 422)
+        end
+      end
+    end
   end
 end
--- spec/api/movies_spec.rb
+++ spec/api/movies_spec.rb
@@ -6,8 +6,8 @@ describe Movies do
   end
 
   describe 'GET /api/movies' do 
-    it 'gets a list of all movies' do 
-      get '/api/movies'
+    it 'gets a list of all movies by day' do 
+      get '/api/movies', day_name: 'monday'
       expect(last_response.status).to eq(200)
       expect(JSON.parse(last_response.body)['movies']).to eq([])
     end
